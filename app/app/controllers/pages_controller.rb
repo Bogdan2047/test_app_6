238e1972460412e7a46e3e628ad844532cca8ce5
Bogdan2047 <bogdan2047@github@.com>
2021-09-09 10:18:36 +0300

    git

---
+++ app/app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+	  def page
+	  end
+	  
+	  def about
+	  end
+ end 
---
+++ app/app/views/pages/node.js
@@ -0,0 +1,22 @@
+let q = [0];
+function getMin(obj){
+   array.forEach(elem => {
+       if (elem > q)
+       q = array
+   });
+   getMin(add)
+
+let dd = {
+    'a':[4,0,3,-3],
+    'b':[2,-4,-5,-4],
+    'c':[1,-7,-3,-1],
+    'd':[11,9,1,4]
+}
+
+function min(obj){
+
+}
+
+for(let key in dd)
+
+console.log(key)
